e492bb49657411fde3b93d3ced2ef23c8bdf910c
zkilleb <zbkilleb@gmail.com>
2018-02-18 16:37:27 -0600

    Update scraper.py
    Added second venue, formatted dates

--- scraper.py
+++ scraper.py
@@ -1,17 +1,16 @@
-#imports
 import bs4 as bs
 import urllib.request
 import MySQLdb
 
-#setting up website to be scraped from
 sauce = urllib.request.urlopen('https://www.firebirdstl.com/listing/').read()
 firebird = bs.BeautifulSoup(sauce, 'lxml')
 
-#variables for iteration
+sauce2 = urllib.request.urlopen('https://www.thereadyroom.com/listing/').read()
+readyRoom = bs.BeautifulSoup(sauce2, 'lxml')
+
 i = 0
 j = 0
 k = 0
-l = 1
 
 #connect to database
 db = MySQLdb.connect(host='host',
@@ -19,33 +18,54 @@ db = MySQLdb.connect(host='host',
                      password='password',
                      db= 'database')
 
-#cursor
 cursor=db.cursor()
 
-#delete all data when compiled to update and avoid duplicates
+#remove all information from dB to avoid duplicates
 cursor.execute("DELETE FROM concert")
 
-#discover number of concerts
+#add headliners, ID and Venue for Firebird
 for artist in firebird.find_all('h1', {"class": "headliners summary"}):
-    i += 1
-
-#populate database with headliner names and ID_Nums
-for artist in firebird.find_all('h1', {"class": "headliners summary"}):
-    cursor.execute("INSERT INTO concert(Headliner, ID_Num) VALUES (%s, %s)", ([artist.text], j))
+    cursor.execute("INSERT INTO concert(Headliner, ID_Num, Venue) VALUES (%s, %s, 'Firebird')", ([artist.text], j))
     j += 1
 
-#delete repeat of data from banner at top of site and add dates to database
+#add dates, remove duplicate and format    
 for date in firebird.find_all('h2', {"class": "dates"}):
-    cursor.execute("DELETE FROM concert WHERE ID_NUM = (%s)", '0')
+    cursor.execute("DELETE FROM concert WHERE ID_NUM = (%s) AND Venue = 'Firebird'", '0')
     cursor.execute("UPDATE concert SET dates = (%s) WHERE ID_NUM = (%s)", ([date.text], k))
     k += 1
 
-#needs if to account for shows that don't have support
-#for support in firebird.find_all('h2', {"class": "supports description"}):
-#    if .find('supports description'):
-#        cursor.execute("UPDATE concert SET Support = (%s) WHERE ID_NUM = (%s)", ([support.text], l))
-#    l += 1
-
-#commit to and close database
+#commit and close dB    
 db.commit()
 db.close()
+
+#connect to database using utf-8
+db = MySQLdb.connect(host='host',
+                     user='user',
+                     password='password',
+                     db= 'database',
+                     use_unicode=True, charset="utf8")
+
+cursor=db.cursor()
+
+#add headliners and other info for ready room
+for artist in readyRoom.find_all('h1', {"class": "headliners summary"}):
+    cursor.execute("INSERT INTO concert(Headliner, ID_Num, Venue) VALUES (%s, %s, 'The Ready Room')", ([artist.text], j))
+    j += 1
+
+#add dates and format
+for date in readyRoom.find_all('h2', {"class": "dates"}):
+    cursor.execute("UPDATE concert SET dates = (%s) WHERE ID_NUM = (%s)", ([date.text], k))
+    cursor.execute("UPDATE concert SET dates = SUBSTRING(dates, LENGTH(dates) - 4)")
+    cursor.execute("UPDATE concert SET dates = REPLACE(dates, '/', '.')")
+    k += 1
+
+#remove extra zero from dates    
+cursor.execute("UPDATE concert SET dates = REPLACE(LTRIM(REPLACE(dates, '0', ' ')), ' ', '0') WHERE dates like '0%'")
+
+#copy to column with datatype decimal to sort by
+cursor.execute("UPDATE concert SET dateInt=dates")
+
+#close Db
+db.commit()
+db.close()
+
